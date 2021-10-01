c4275677c4650287c836d2e3e9f39a31ddc6e5c2
luke-a-rogers <luke.allister.rogers@gmail.com>
2021-09-30 20:26:08 -0700

    updated with utility functions; mod() not currently used

--- sablefishmovement/R/utils.R
+++ sablefishmovement/R/utils.R
@@ -1,3 +1,27 @@
+#' Modulo Function
+#'
+#' @param a [numeric()] [vector()]
+#' @param b [numeric()] [vector()]
+#' @param zeros [logical()] replace zeros by \code{b}?
+#'
+#' @return [numeric()]
+#' @export
+#'
+#' @examples
+#' # Standard modulo operation
+#' mod(1:10, 8)
+#'
+#' # Replace zeros by \code{b}
+#' mod(1:10, 8, zeros = FALSE)
+#'
+mod <- function (a, b, zeros = TRUE) {
+  ans <- a - (b * floor(a / b))
+  if (!zeros) {
+    ans[which(ans == 0)] <- b
+  }
+  return(ans)
+}
+
 #' Read File From Path
 #'
 #' @param path [character()] file path
@@ -9,6 +33,23 @@ read_from_path <- function (path) {
   envir <- environment()
   data_name <- load(path, envir = envir)
   get(data_name)
+}
+
+#' Convert Released Time Unit To Time Steps Per Year
+#'
+#' @param unit [character()] one of \code{'year'}, \code{'quarter'}, or
+#'   \code{'month'}.
+#'
+#' @return [numeric()] number of time steps per year
+#' @export
+#'
+#' @examples
+#' unit_to_time("year")
+#' unit_to_time("quarter")
+#' unit_to_time("month")
+#'
+unit_to_time <- function (unit) {
+  c(1L, 4L, 12L)[which(c("year", "quarter", "month") == unit)]
 }
 
 #' Write An Object To Data And Return The Path
