d8284d59281b4bd3b8123f61796333c453d414e9
Luke A. Rogers <luke.allister.rogers@gmail.com>
2023-07-14 15:48:41 -0700

    updated to include region_short

--- data-raw/sf_omregions.R
+++ data-raw/sf_omregions.R
@@ -1,11 +1,13 @@
 # Source utils
 source("R/utils.R")
+library(magrittr)
 
 # Define path
 path <- "~/github/sablefish-shapefiles/data/sf_omregions.rda"
 
 # Assign value
-sf_omregions <- read_from_path(path = path)
+sf_omregions <- read_from_path(path = path) %>%
+  dplyr::mutate(region_short = omregion_short)
 
 # Write to data/
 write_data(sf_omregions)
